ca7ff23154033e3d1ed564a13152d3e011d478e5
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-15 02:49:59 -0300

    enhance alpha.lua

--- lua/maximowehncke/plugins/alpha.lua
+++ lua/maximowehncke/plugins/alpha.lua
@@ -1,39 +1,39 @@
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+	{
+		"MaximilianLloyd/ascii.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		dependencies = {
+			"MaximilianLloyd/ascii.nvim",
+		},
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
 
-		-- Set header
-		dashboard.section.header.val = {
-			"                                                     ",
-			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-			"                                                     ",
-		}
+			dashboard.section.header.val = require("ascii").art.text.neovim.sharp
 
-		-- NOTE: 1 commit 1
+			-- Set menu
+			dashboard.section.buttons.val = {
+				dashboard.button("e", " New File", "<cmd>ene<CR>"),
+				dashboard.button("SPC ee", " Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+				dashboard.button("SPC ff", "󰱼 Find File", "<cmd>Telescope find_files<CR>"),
+				dashboard.button("SPC fs", " Find Word", "<cmd>Telescope live_grep<CR>"),
+				dashboard.button(
+					"SPC wr",
+					"󰁯 Restore Session For Current Directory",
+					"<cmd>AutoSession restore<CR>"
+				),
+				dashboard.button("q", " Quit NVIM", "<cmd>qa<CR>"),
+			}
 
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>AutoSession restore<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-		}
-		-- NOTE: 2 coomit 2
+			alpha.setup(dashboard.opts)
 
-		-- Send config to alpha
-		alpha.setup(dashboard.opts)
-
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	end,
+			vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+		end,
+	},
 }
