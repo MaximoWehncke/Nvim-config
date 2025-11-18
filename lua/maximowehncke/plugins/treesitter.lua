c2a468fad1351df4f38ef52f823aa508eaf356d7
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-18 14:19:03 -0300

    feat(plugins):treesitter text objects
    i added it as a second table to the original treesitter file

--- lua/maximowehncke/plugins/treesitter.lua
+++ lua/maximowehncke/plugins/treesitter.lua
@@ -1,58 +1,139 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			-- import nvim-treesitter plugin
+			local treesitter = require("nvim-treesitter.configs")
 
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"java",
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"c",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
+			-- configure treesitter
+			treesitter.setup({ -- enable syntax highlighting
+				highlight = {
+					enable = true,
 				},
-			},
-		})
-	end,
+				-- enable indentation
+				indent = { enable = true },
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = {
+					enable = true,
+				},
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"java",
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"yaml",
+					"html",
+					"css",
+					"prisma",
+					"markdown",
+					"markdown_inline",
+					"svelte",
+					"graphql",
+					"bash",
+					"lua",
+					"vim",
+					"dockerfile",
+					"gitignore",
+					"query",
+					"vimdoc",
+					"c",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+		end,
+	},
+	{
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			dependencies = { "nvim-treesitter/nvim-treesitter" },
+			config = function()
+				require("nvim-treesitter.configs").setup({
+					textobjects = {
+						select = {
+							enable = true,
+							-- Automatically jump forward to textobj, similar to targets.vim
+							lookahead = true,
+							keymaps = {
+								-- You can use the capture groups defined in textobjects.scm
+								["af"] = "@function.outer",
+								["if"] = "@function.inner",
+								["ac"] = "@class.outer",
+								["ic"] = "@class.inner",
+								["aa"] = "@parameter.outer",
+								["ia"] = "@parameter.inner",
+								["ab"] = "@block.outer",
+								["ib"] = "@block.inner",
+								["al"] = "@loop.outer",
+								["il"] = "@loop.inner",
+								["ai"] = "@conditional.outer",
+								["ii"] = "@conditional.inner",
+								["a/"] = "@comment.outer",
+								["i/"] = "@comment.inner",
+							},
+						},
+
+						move = {
+							enable = true,
+							set_jumps = true, -- whether to set jumps in the jumplist
+							goto_next_start = {
+								["]f"] = "@function.outer",
+								["]c"] = "@class.outer",
+								["]a"] = "@parameter.inner",
+							},
+							goto_next_end = {
+								["]F"] = "@function.outer",
+								["]C"] = "@class.outer",
+								["]A"] = "@parameter.inner",
+							},
+							goto_previous_start = {
+								["[f"] = "@function.outer",
+								["[c"] = "@class.outer",
+								["[a"] = "@parameter.inner",
+							},
+							goto_previous_end = {
+								["[F"] = "@function.outer",
+								["[C"] = "@class.outer",
+								["[A"] = "@parameter.inner",
+							},
+						},
+
+						swap = {
+							enable = true,
+							swap_next = {
+								["<leader>a"] = "@parameter.inner",
+							},
+							swap_previous = {
+								["<leader>A"] = "@parameter.inner",
+							},
+						},
+
+						lsp_interop = {
+							enable = true,
+							border = "none",
+							floating_preview_opts = {},
+							peek_definition_code = {
+								["<leader>df"] = "@function.outer",
+								["<leader>dF"] = "@class.outer",
+							},
+						},
+					},
+				})
+			end,
+		},
+	},
 }
