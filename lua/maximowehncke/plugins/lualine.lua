32539c0ae9c76bd9a28a1100194a04d7ff693e78
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-18 23:29:57 -0300

    enhanced lualine

--- lua/maximowehncke/plugins/lualine.lua
+++ lua/maximowehncke/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		local lazy_status = require("lazy.status")
 
 		local colors = {
 			blue = "#65D1FF",
@@ -18,47 +18,16 @@ return {
 			inactive_fg = "#606060",
 		}
 
-		local my_lualine_theme = {
-			normal = {
-				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			insert = {
-				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			visual = {
-				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			command = {
-				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			replace = {
-				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			inactive = {
-				a = { bg = colors.dark_bg, fg = colors.inactive_fg, gui = "bold" },
-				b = { bg = colors.dark_bg, fg = colors.inactive_fg },
-				c = { bg = colors.dark_bg, fg = colors.inactive_fg },
-			},
-		}
-
 		-- configure lualine with modified theme
 		lualine.setup({
 			options = {
-				theme = my_lualine_theme,
+				theme = "catppuccin",
 				disabled_filetypes = { "packer", "NvimTree" },
 				section_separators = { left = "", right = "" },
-				component_separators = { left = "|", right = "|" },
+				component_separators = { left = "", right = "" },
 				globalstatus = false,
+				padding = 4,
+				always_divide_middle = false,
 			},
 			sections = {
 				lualine_a = {},
@@ -76,29 +45,32 @@ return {
 				lualine_y = {},
 				lualine_z = {},
 			},
-
 			winbar = {
 				lualine_a = { "mode" },
 				lualine_b = { "filename" },
-				lualine_c = { "diff" },
-				lualine_x = {
+				lualine_c = { "branch" },
+				lualine_x = { "diff" },
+				lualine_y = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
 						color = { fg = "#ff9e64" },
 					},
 				},
-				lualine_y = { "filename" },
-				lualine_z = { "branch" },
+				lualine_z = {},
 			},
 			inactive_winbar = {
-				lualine_a = { "filename" },
-				lualine_b = { "diff" },
+				lualine_a = {},
+				lualine_b = { "filename" },
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
 			},
-
 			extensions = {},
 		})
 
+		vim.opt.showtabline = 1
 		vim.opt.laststatus = 0
 	end,
 }
