76413de31dd376ae03bfa4be1c5ce18e243f68bb
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-21 09:21:07 -0300

    feat(plugins): add copilot completions

--- lua/maximowehncke/plugins/autocompletion.lua
+++ lua/maximowehncke/plugins/autocompletion.lua
@@ -97,4 +97,13 @@ return {
 			signature = { enabled = true },
 		},
 	},
+	{
+		"github/copilot.vim",
+		event = "InsertEnter",
+
+		config = function()
+			vim.g.copilot_no_tab_map = true
+			vim.api.nvim_set_keymap("i", "<S-Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		end,
+	},
 }
