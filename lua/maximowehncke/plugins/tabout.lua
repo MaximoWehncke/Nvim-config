a343de5ddbbf2d88a08f9632039adbca355700f8
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-23 17:37:54 -0300

    fix: remove unused tabout dependencies

--- lua/maximowehncke/plugins/tabout.lua
+++ lua/maximowehncke/plugins/tabout.lua
@@ -27,10 +27,8 @@ return {
 		dependencies = { -- These are optional
 			"nvim-treesitter/nvim-treesitter",
 			"L3MON4D3/LuaSnip",
-			"hrsh7th/nvim-cmp",
 		},
-
-		opt = true, -- Set this to true if the plugin is optional
+		-- opt = true, -- Set this to true if the plugin is optional
 		event = "InsertCharPre", -- Set the event to 'InsertCharPre' for better compatibility
 		priority = 1000,
 	},
