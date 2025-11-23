85ed9acacf267404ab44b22063afed7ce27e9ad1
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-23 17:33:06 -0300

    fix: comment.nvim for inline C comments

--- lua/maximowehncke/plugins/comment.lua
+++ lua/maximowehncke/plugins/comment.lua
@@ -1,19 +1,30 @@
 return {
 	"numToStr/Comment.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
-	},
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 	config = function()
-		-- import comment plugin safely
 		local comment = require("Comment")
+		local ts_pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-		-- enable comment
 		comment.setup({
-			-- for commenting tsx, jsx, svelte, html files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+			pre_hook = function(ctx)
+				local ft = vim.bo.filetype
+
+				-- Only use context-commentstring for filetypes that need dynamic commentstring
+				local dynamic = {
+					tsx = true,
+					jsx = true,
+					javascriptreact = true,
+					typescriptreact = true,
+					svelte = true,
+					html = true,
+					vue = true,
+				}
+
+				if dynamic[ft] then
+					return ts_pre_hook(ctx)
+				end
+			end,
 		})
 	end,
 }
