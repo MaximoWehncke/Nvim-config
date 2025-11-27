74f6829ca5f2562fd26212657e1a7c4d9055a847
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-27 01:40:32 -0300

    feat: smart splits plugin

---
+++ lua/maximowehncke/plugins/smart-splits.lua
@@ -0,0 +1,31 @@
+return {
+	"mrjones2014/smart-splits.nvim",
+	config = function()
+		require("smart-splits").setup({
+
+			-- Configuration
+			resize_mode = {
+				silent = true,
+				hooks = {
+					on_enter = function()
+						vim.notify("Entering resize mode", vim.log.levels.INFO)
+					end,
+					on_leave = function()
+						vim.notify("Exiting resize mode", vim.log.levels.INFO)
+					end,
+				},
+			},
+		})
+
+		-- Resize windows with r prefix (like your tmux setup)
+		vim.keymap.set("n", "rh", require("smart-splits").resize_left, { desc = "Resize left" })
+		vim.keymap.set("n", "rj", require("smart-splits").resize_down, { desc = "Resize down" })
+		vim.keymap.set("n", "rk", require("smart-splits").resize_up, { desc = "Resize up" })
+		vim.keymap.set("n", "rl", require("smart-splits").resize_right, { desc = "Resize right" })
+
+		vim.keymap.set("n", "<leader>wh", require("smart-splits").swap_buf_left, { desc = "Swap with left" })
+		vim.keymap.set("n", "<leader>wj", require("smart-splits").swap_buf_down, { desc = "Swap with bottom" })
+		vim.keymap.set("n", "<leader>wk", require("smart-splits").swap_buf_up, { desc = "Swap with top" })
+		vim.keymap.set("n", "<leader>wl", require("smart-splits").swap_buf_right, { desc = "Swap with right" })
+	end,
+}
