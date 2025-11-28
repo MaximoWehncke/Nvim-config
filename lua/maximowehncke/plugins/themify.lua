4002ce8b046c62afc265cd2ece0d593ebe64a2cc
MaximoWehncke <mwehncke@itba.edu.ar>
2025-11-28 16:27:30 -0300

    rose-pine colorscheme

--- lua/maximowehncke/plugins/themify.lua
+++ lua/maximowehncke/plugins/themify.lua
@@ -62,6 +62,14 @@ return {
 						require("gruvbox").setup()
 					end,
 				},
+				{
+					"rose-pine/neovim",
+					name = "rose-pine",
+					config = function()
+						vim.cmd("colorscheme rose-pine")
+					end,
+				},
+				{ "sainnhe/sonokai" },
 				"default",
 			})
 		end,
